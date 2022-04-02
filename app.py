01e68d2d941810fd9f13bed7cebf02e3b222d066
Khondamir Rustamov <69417652+KhondamirRustamov@users.noreply.github.com>
2022-04-02 22:07:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect
 import telegram
 from credentials import bot_token, URL
+from flask_sqlalchemy import SQLAlchemy
 
 global bot
 global TOKEN
@@ -8,6 +9,18 @@ TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    chat = db.Column(db.Text)
+    mode = db.Column(db.Text, default = 'nothing')
+
+    def __repr__(self):
+        return '<Flora %r>' % self.id
 
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
@@ -17,11 +30,25 @@ def respond():
 
    chat_id = update.message.chat.id
    msg_id = update.message.message_id
-
+   user_id = update.message.chat.first_name
    # Telegram understands UTF-8, so encode text for unicode compatibility
    text = update.message.text.encode('utf-8').decode()
    # for debugging purposes only
    print("got text message :", text)
+   users = User.query.all()
+   a = 0
+   for i in users:
+       if str(chat_id) in str(i.chat):
+           a = 1
+   if a == 0:
+       user = User(chat=chat_id,
+                   user=user_id)
+       try:
+           db.session.add(user)
+           db.session.commit()
+       except:
+           print('no')
+
    # the first time you chat with the bot AKA the welcoming message
    if text == "/start":
        # print the welcoming message
@@ -29,6 +56,54 @@ def respond():
        Assalomu aleykum"""
        # send the welcoming message
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+   
+   elif text == "/onto":
+       user = User.query.get(chat=chat_id, user=user_id)
+       user.mode = 'onto'
+       try:
+           db.session.commit()
+       except:
+           print('no')
+       bot_help = 'Rejim: ontogenetik parametrlarni tahlili'
+       bot.sendMessage(chat_id=chat_id, text=bot_help, reply_to_message_id=msg_id)
+
+   elif text == "/morpho":
+       user = User.query.get(chat=chat_id, user=user_id)
+       user.mode = 'morpho'
+       try:
+           db.session.commit()
+       except:
+           print('no')
+       bot_help = 'Rejim: morfologik parametrlarni tahlili'
+       bot.sendMessage(chat_id=chat_id, text=bot_help, reply_to_message_id=msg_id)
+    
+   else:
+       user = User.query.get(chat=chat_id, user=user_id)
+       if user.mode == 'onto':
+           parameters = text.split(' ')
+           try:
+               delta_coef = [0.0025, 0.0067, 0.0180, 0.0474, 0.1192, 0.2689, 0.5, 0.7311, 0.8808, 0.8808, 0.9820]
+               omega_coef = [0.0099, 0.0266, 0.0707, 0.1807, 0.4200, 0.7864, 1.0, 0.7864, 0.4200, 0.1807, 0.0707]
+               Delta = sum(i * m for i, m in zip(parameters, delta_coef)) / sum(parameters)
+               Omega = sum(i * m for i, m in zip(parameters, omega_coef)) / sum(parameters)
+               if Omega < 0.60 and Delta < 0.35:
+                   bot_help = "Ценопопуляция молодая, Δ=%s, ω=%s" % (str(Delta), str(Omega))  # molodoy
+               elif Omega >= 0.60 and Delta < 0.35:
+                   bot_help = "Ценопопуляция зреющая, Δ=%s, ω=%s" % (str(Delta), str(Omega))  # zreyushiy
+               elif Omega < 0.70 and 0.35 <= Delta < 0.55:
+                   bot_help = "Ценопопуляция переходная, Δ=%s, ω=%s" % (str(Delta), str(Omega))  # perehodniy
+               elif Omega >= 0.70 and 0.35 <= Delta < 0.55:
+                   bot_help = "Ценопопуляция зрелая, Δ=%s, ω=%s" % (str(Delta), str(Omega))  # zreliy
+               elif Omega < 0.60 and Delta >= 0.55:
+                   bot_help = "Ценопопуляция старая, Δ=%s, ω=%s" % (str(Delta), str(Omega))  # stariy
+               elif Omega > 0.60 and Delta >= 0.55:
+                   bot_help = "Ценопопуляция стареющая, Δ=%s, ω=%s" % (str(Delta), str(Omega))  # stareyushiy
+               else:
+                   bot_help = "Ошибка! Дельта и Омега числа меньше 1"
+         except:
+               bot_help = '''Общее количество особей не совпадает с количеством введенных данных(сумма
+                           онтогенетических состояний не равна введеному количеству всех особей)'''
+         bot.sendMessage(chat_id=chat_id, text=bot_help, reply_to_message_id=msg_id)
    return 'ok'
 
 
@@ -42,7 +117,26 @@ def set_webhook():
 
 @app.route('/')
 def index():
-    return '.'
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+
+@app.route('/send', methods=['POST', 'GET'])
+def send():
+    if request.method == 'POST':
+        TOKEN = bot_token
+        bot = telegram.Bot(token=TOKEN)
+        message = str(request.form['message'])
+        users = User.query.all()
+        for i in users:
+            try:
+                chat_id = i.chat
+                bot.sendMessage(chat_id=chat_id, text=message)
+            except:
+                pass
+        return redirect("/")
+    else:
+        return render_template("send.html")
    
    
 if __name__ == '__main__':
