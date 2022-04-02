59dc99e2abf21fcfb0de48bfae971af6fb961135
Khondamir Rustamov <69417652+KhondamirRustamov@users.noreply.github.com>
2022-04-02 20:21:26 +0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 bot_token = "5103389741:AAGwj2PCBFTRpWarXhu-JcX737tH2U24G6o"
-bot_user_name = "PoStNiche - Population Statistics"
+bot_user_name = "postniche_bot"
 URL = "https://postniche-bot.herokuapp.com/"
