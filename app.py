87b2f01025cadfac5d0de34031ac0ec856f97383
Khondamir Rustamov <69417652+KhondamirRustamov@users.noreply.github.com>
2022-04-02 20:48:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -51,9 +51,7 @@ def respond():
 
     if text == "/start":
         # print the welcoming message
-        bot_welcome = '''
-Assalomu aleykum
-    '''
+        bot_welcome = '''Assalomu aleykum'''
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
